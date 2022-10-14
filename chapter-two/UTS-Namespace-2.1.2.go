4402a4d2cd341a2ab69cecb15991bfc21af6b893
bitleef <2425217208@qq.com>
2022-10-15 02:59:34 +0800

    UTS_Namespace

---
+++ chapter-two/UTS-Namespace-2.1.2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func UTSNamespace() (err error) {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err = cmd.Run(); err != nil {
+		fmt.Println(err)
+	}
+	return err
+}
---
+++ chapter-two/main-chapter-two.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+/*
+* 采用带参数执行对应的函数，每次输入的参数有且只能有一个，相应的参数如下：
+* 1. UTS_Namespace 隔离nodename和domainname两个系统标识
+ */
+var CallFunctionName string
+
+const (
+	UTS_NAMESPACE = "UTS_Namespace"
+)
+
+func main() {
+	var err error
+	for idx, arg := range os.Args {
+		if idx == 2 {
+			fmt.Println("too many parameters:", os.Args)
+			return
+		} else if idx == 1 {
+			CallFunctionName = strings.Replace(arg, "-", "_", -1)
+			fmt.Println("call function is：", CallFunctionName)
+		}
+	}
+
+	switch CallFunctionName {
+	case UTS_NAMESPACE:
+		err = UTSNamespace()
+	default:
+		fmt.Println("call function is NULL, please input parameter")
+	}
+	if err != nil {
+		fmt.Println(CallFunctionName, "failed to run")
+	}
+}
