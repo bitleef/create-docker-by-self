25f519307b723e90888e43de35ba8b6b3193eb22
bitleef <2425217208@qq.com>
2023-02-24 01:47:10 +0800

    namespace test

--- chapter-two/main-chapter-two.go
+++ chapter-two/main-chapter-two.go
@@ -9,11 +9,18 @@ import (
 /*
 * 采用带参数执行对应的函数，每次输入的参数有且只能有一个，相应的参数如下：
 * 1. UTS_Namespace 隔离nodename和domainname两个系统标识
+* 2. IPC_Namespace 隔离System V IPC和POSIX message queues
+* 3. PID_Namespace 隔离进程ID
+* 4. Mount_Namespace 隔离各个进程看到的挂载点视图
+* 5.
  */
 var CallFunctionName string
 
 const (
-	UTS_NAMESPACE = "UTS_Namespace"
+	UTS_NAMESPACE   = "UTS_Namespace"
+	IPC_NAMESPACE   = "IPC_Namespace"
+	PID_NAMESPACE   = "PID_Namespace"
+	MOUNT_NAMESPACE = "Mount_Namespace"
 )
 
 func main() {
@@ -24,13 +31,31 @@ func main() {
 			return
 		} else if idx == 1 {
 			CallFunctionName = strings.Replace(arg, "-", "_", -1)
-			fmt.Println("call function is：", CallFunctionName)
+			fmt.Println("call function is:", CallFunctionName)
 		}
 	}
 
 	switch CallFunctionName {
 	case UTS_NAMESPACE:
 		err = UTS_Namespace()
+		if err != nil {
+			fmt.Printf("%s failed to run, err is %v", CallFunctionName, err)
+		}
+	case IPC_NAMESPACE:
+		err = IPC_Namespace()
+		if err != nil {
+			fmt.Printf("%s failed to run, err is %v", CallFunctionName, err)
+		}
+	case PID_NAMESPACE:
+		err = PID_Namespace()
+		if err != nil {
+			fmt.Printf("%s failed to run, err is %v", CallFunctionName, err)
+		}
+	case MOUNT_NAMESPACE:
+		err = Mount_Namespace()
+		if err != nil {
+			fmt.Printf("%s failed to run, err is %v", CallFunctionName, err)
+		}
 	default:
 		fmt.Println("call function is NULL, please input parameter")
 	}
